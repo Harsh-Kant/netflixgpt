e3bddeb250fe049b8b5c64ba5627a3f91a53322f
Harsh-Kant <harshkant2020@gmail.com>
2025-11-07 02:12:30 +0530

    login page

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="absolute z-30 top-6 left-8 w-20 object-contain">
+        <img src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico" alt="logo" />
+    </div>
+  )
+}
+
+export default Header
