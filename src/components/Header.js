1f3a3f79ffeda8d6dc9744295b297992fd8e9253
Harsh-Kant <harshkant2020@gmail.com>
2025-11-16 16:59:05 +0530

    TMDB api data fetch and build trailer container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,39 @@
-import { signOut } from 'firebase/auth';
-import React from 'react'
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import React, { useEffect } from 'react'
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
+import { logo } from '../utils/constant';
 
 const Header = () => {
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const user = useSelector(store=>store.user)
   const handleSignout = () => {
     signOut(auth).then(() => {
-      navigate('/')
     }).catch((error) => {
       navigate('/error')
     });
   }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const {uid, email, displayName, photoURL} = user;
+        dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL:photoURL}))
+        navigate('/browse')
+      } else {
+        dispatch(removeUser())
+        navigate('/')
+      }
+    });
+    return ()=>unsubscribe()
+  }, []);
+
   return (
     <div className="absolute z-10 px-8 py-2 object-contain bg-gradient-to-b from-black w-screen flex justify-between">
-        <img className='w-22' src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico" alt="logo" />
+        <img className='w-22' src={logo} alt="logo" />
         {user && (<div className='flex p-2'>
           <img className='h-12 w-12 p-2' src={user?.photoURL} alt="user" />
           <button className='font-bold text-white' onClick={handleSignout}>Sign Out</button>
---
+++ src/utils/constant.js
@@ -0,0 +1,9 @@
+export const logo = "https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTUxMGI5ZTc4NzI5OGQwNzMxN2Y4NTU5OTcxMTkzMCIsIm5iZiI6MTc2MzIwMTEzMC45NCwic3ViIjoiNjkxODUwNmFkMWRhN2VkNmEyODc2ZmZlIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.B0eqQR9xJ2aMOrvzQfGSV-U6qlnGDeVhx16w8FzfKUk'
+    }
+  };
